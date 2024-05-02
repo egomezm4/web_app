83f06a30bea87c14d5479ffdba504c5cb4ce167b
egomezm4 <egomez@popoyan.com>
2024-05-02 10:42:41 -0600

    otro

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD Saludos SONNAR</title>
+    <title>CRUD Saludoxs SONNAR</title>
     <style>
    body {
             font-family: Arial, sans-serif;
